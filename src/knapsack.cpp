bd3311c8a28a1c7c6fb6002605c5c63fca19f5f1
Joao Pedro <joopedrof.silva@gmail.com>
2023-01-06 19:24:40 -0300

    Started developing branch and bound solution.

--- src/knapsack.cpp
+++ src/knapsack.cpp
@@ -1,29 +1,60 @@
 #include <vector>
 #include <iostream>
-#include <string>
 #include <fstream>
 #include <algorithm>
+#include <queue>
+#include <map>
 
 using namespace std;
 
-int main(int argc, char* argv[]){
-    string fName = argv[1];
-    fstream f(fName, fstream::in | fstream::out);
-    int numItems, capacity;
-    f >> numItems; f >> capacity;
-    vector<bool> selectedItens(numItems, 0);
+struct bnbNode{
+    bnbNode* parent;
+    int expectedValue;
+    int chosenValuesSum;
+    bool selectedItem;
 
-    vector<pair<int, int>> itens(numItems, make_pair(0, 0));
-    for(int i = 0; i < numItems; i++){
-            f >> itens[i].second;
-            f >> itens[i].first;
+    bnbNode(bnbNode* p, int expected, bool selected) : parent(p), expectedValue(expected),
+                                                        selectedItem(selected) {}
+};
+
+int bnbSolver(vector<pair<int, int>>& items, int capacity, 
+                            vector<bool>& selecteditems){
+    
+    int numItems = items.size();
+    //Map with all values from linear relaxation
+    map<int, vector<int>> relaxationMap;
+    for(int i = 0; i < items.size(); i++){
+        float value = (float)items[i].second / items[i].first;
+        auto it = relaxationMap.find(value);
+        if(it != relaxationMap.end()){
+            it->second.push_back(i); 
+        }
+        else{
+            vector<int> vec; vec.push_back(i);
+            relaxationMap.insert(make_pair(value, vec));
+        }
     }
+    int initialExpect = 0;
+    int remWeight = capacity;
+    for(auto it = relaxationMap.begin(); it != relaxationMap.end(); it++){
+        for(int i : it->second){
+            int itemWeight = items[i].first;
+            if(itemWeight > remWeight){
+                
+            }
+        }
+    }
+}
 
+int dynamicProgrammingSolver(vector<pair<int, int>>& items, int capacity, 
+                            vector<bool>& selecteditems){
+
+    int numItems = items.size();
     vector<vector<int>> mtx(capacity + 1, vector<int>(numItems + 1, 0));
     for(int j = 1; j <= numItems; j++){
-        int itemWeight = itens[j - 1].first;
+        int itemWeight = items[j - 1].first;
         int i = itemWeight;
-        int itemVal = itens[j - 1].second;
+        int itemVal = items[j - 1].second;
 
         //Completing the collum
         for(int k = 0; k < i; k ++){
@@ -55,14 +86,31 @@ int main(int argc, char* argv[]){
             j --;
         }
         else{
-            selectedItens[j - 1] = 1;
-            int itemWeight = itens[j - 1].first;
+            selecteditems[j - 1] = 1;
+            int itemWeight = items[j - 1].first;
             i -= itemWeight; j--;
         }
     }
+    return optimal;
+}
 
-    for(int i = 0; i < selectedItens.size(); i++){
-        cout << selectedItens[i] << " ";
+int main(int argc, char* argv[]){
+    string fName = argv[1];
+    fstream f(fName, fstream::in | fstream::out);
+    int numItems, capacity;
+    f >> numItems; f >> capacity;
+    vector<bool> selecteditems(numItems, 0);
+
+    vector<pair<int, int>> items(numItems, make_pair(0, 0));
+    for(int i = 0; i < numItems; i++){
+            f >> items[i].second;
+            f >> items[i].first;
+    }
+
+    
+
+    for(int i = 0; i < selecteditems.size(); i++){
+        cout << selecteditems[i] << " ";
     }
     cout << endl;
 }
